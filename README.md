d330f4c345b556be9d0a75d252b3cf7cf0f8eae6
ArekRGW <arkadiusz.pawlak.web@gmail.com>
2020-06-04 16:49:39 -0400

    Scripts finished

---
+++ README.md
@@ -0,0 +1,6 @@
+### Scirpts for RPI for installing supla-virtual-device and LYWSD03MMC.py and config everything
+
+#### Run in order
+1. ./virtual-device-install
+2. ./supervisor-install
+3. ./mi-temp-install-and-config
---
+++ mi-temp-install-and-config.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt install python3 bluez python3-pip -y
+sudo pip3 install bluepy -y
+
+mkdir -p ~/mi-temp
+cp ./save-to-file.sh ~/mi-temp
+cd ~/mi-temp/
+chmod +x save-to-file.sh
+
+sudo wget https://raw.githubusercontent.com/JsBergbau/MiTemperature2/master/LYWSD03MMC.py
+
+n=0
+
+# need an input file with MAC adresses
+while read line; do
+    echo "[program:mi-temp-$n]
+command=python3 ./LYWSD03MMC.py --device $line --round --debounce --name sensor$n --callback save-to-file.sh
+directory=/home/pi/mi-temp
+autostart=true
+autorestart=true
+user=pi
+" | sudo tee "/etc/supervisor/conf.d/mi-th$n.conf"
+n=$((n+1))
+done
